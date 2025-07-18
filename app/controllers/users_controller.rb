a6a42ada5081e3f60f3785d60a76220f7362fd38
aym0546 <ciany.f@gmail.com>
2025-07-18 22:25:30 +0900

    Remove edit/update views and routes; handle avatar update in show view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,26 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_user, only: [:show, :update]
 
   def show
-    @user = current_user
-  end
-
-  def edit
   end
 
   def update
+    if @user.update(user_params)
+      redirect_to users_path, notice: 'プロフィール画像が更新されました'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :show
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:avatar)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,11 @@
 Rails.application.routes.draw do
-  get "users/show"
-  get "users/edit"
-  get "users/update"
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   root 'posts#index'
 
-  # プロフィール表示・編集用のカスタムコントローラ
-  resource :users, only: [:show, :edit, :update]
+  # プロフィール表示・更新用のカスタムコントローラ
+  resource :users, only: [:show, :update]
 
 end
