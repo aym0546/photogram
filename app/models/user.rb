fead920115a6dd5458724b700a39aa5900ce0650
aym0546 <ciany.f@gmail.com>
2025-07-19 07:49:08 +0900

    Add avatar image support to User with Active Storage

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  has_one_attached :avatar
+
   validates :account, presence: true,
                       length: { maximum: 25 },
                       uniqueness: true
+
+  def avatar_img
+    if self.avatar&.attached?
+      self.avatar
+    else
+      'default.svg'
+    end
+  end
+
 end
