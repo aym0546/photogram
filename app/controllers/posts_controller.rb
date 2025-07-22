6d2700ff658c705c06b6051e0774385e7f634fbf
aym0546 <ciany.f@gmail.com>
2025-07-22 12:08:29 +0900

    Enable multiple image upload in posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,28 @@
 class PostsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create]
   def index
-    @post = Post.first
+    @posts = Post.all
+  end
+
+  def new
+    @post = current_user.posts.build
+  end
+
+  def create
+    @post = current_user.posts.build(post_params)
+    if @post.save
+      redirect_to root_path, notice: 'ポストされました'
+    else
+      flash.now[:error] = 'ポストされませんでした'
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  # @post.image.attach(params[:images])：画像追加
+  # @post.image.attached?：画像が添付されているか
+  def post_params
+    params.expect(post: [ :user_id, :caption, images: [] ])
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -14,5 +14,6 @@
 #
 
 class Post < ApplicationRecord
+  has_many_attached :images
   belongs_to :user
 end
