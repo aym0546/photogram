24298091afe3c39a5873b15558b5f7a255786d84
aym0546 <ciany.f@gmail.com>
2025-07-23 14:31:53 +0900

    Add associations to Like model for User and Post

--- app/models/post.rb
+++ app/models/post.rb
@@ -15,6 +15,9 @@
 
 class Post < ApplicationRecord
   has_many_attached :images
+
+  has_many :likes, dependent: :destroy
+
   belongs_to :user
 
   validates :caption, length: { maximum: 400 }
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,9 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one_attached :avatar
-  has_many :posts
+
+  has_many :posts, dependent: :destroy
+  has_many :likes, dependent: :destroy
 
   validates :account, presence: true,
                       length: { maximum: 25 },
