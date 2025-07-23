3c5ad67dd0012910fa55f33d4d23ac02e7a3b7c9
aym0546 <ciany.f@gmail.com>
2025-07-23 11:11:17 +0900

    feat: generate Stimulus controller for image preview

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,8 +1,9 @@
-
 import { Application } from '@hotwired/stimulus';
 import HelloController from './hello_controller';
+import ImagePreviewController from './image_preview_controller';
 
 const application = Application.start();
 application.register('hello', HelloController);
+application.register('image-preview', ImagePreviewController);
 
 export { application };
