ae77ed7eae999bf2a9967465ee45e13d139f2ac6
aym0546 <ciany.f@gmail.com>
2025-07-23 14:30:51 +0900

    Generate Like model with references to User and Post

---
+++ app/models/like.rb
@@ -0,0 +1,18 @@
+# == Schema Information
+#
+# Table name: likes
+#
+#  id         :integer          not null, primary key
+#  user_id    :integer          not null
+#  post_id    :integer          not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+# Indexes
+#
+#  index_likes_on_post_id  (post_id)
+#  index_likes_on_user_id  (user_id)
+#
+
+class Like < ApplicationRecord
+end
