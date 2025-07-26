479cef71ddfdabbf3818ea7057f37e2253282343
aym0546 <ciany.f@gmail.com>
2025-07-27 00:28:13 +0900

    Add Relationship model and migration for follow feature

---
+++ app/models/relationship.rb
@@ -0,0 +1,21 @@
+# == Schema Information
+#
+# Table name: relationships
+#
+#  id           :integer          not null, primary key
+#  follower_id  :integer          not null
+#  following_id :integer          not null
+#  created_at   :datetime         not null
+#  updated_at   :datetime         not null
+#
+# Indexes
+#
+#  index_relationships_on_follower_id                   (follower_id)
+#  index_relationships_on_follower_id_and_following_id  (follower_id,following_id) UNIQUE
+#  index_relationships_on_following_id                  (following_id)
+#
+
+class Relationship < ApplicationRecord
+  belongs_to :follower
+  belongs_to :following
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_07_23_125732) do
+ActiveRecord::Schema[8.0].define(version: 2025_07_26_150709) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -69,6 +69,16 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_23_125732) do
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
+  create_table "relationships", force: :cascade do |t|
+    t.bigint "follower_id", null: false
+    t.bigint "following_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["follower_id", "following_id"], name: "index_relationships_on_follower_id_and_following_id", unique: true
+    t.index ["follower_id"], name: "index_relationships_on_follower_id"
+    t.index ["following_id"], name: "index_relationships_on_following_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -88,4 +98,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_07_23_125732) do
   add_foreign_key "comments", "posts"
   add_foreign_key "comments", "users"
   add_foreign_key "posts", "users"
+  add_foreign_key "relationships", "users", column: "follower_id"
+  add_foreign_key "relationships", "users", column: "following_id"
 end
