b9db60c9195dbe4465f0c62adcf6a5b94fcbbaf8
aym0546 <ciany.f@gmail.com>
2025-07-28 03:08:03 +0900

    Generate Stimulus controller for follow feature

---
+++ app/javascript/controllers/follow_controller.js
@@ -0,0 +1,9 @@
+import { Controller } from '@hotwired/stimulus';
+import axios from 'axios';
+
+// Connects to data-controller="follow"
+export default class extends Controller {
+
+  connect() {
+  }
+}
