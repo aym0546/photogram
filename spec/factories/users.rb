2c4ecbdc4ec009d957822382f617951b1f103a2f
aym0546 <ciany.f@gmail.com>
2025-07-30 14:45:35 +0900

    Add avatar attachment trait to user factory for testing

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -3,5 +3,18 @@ FactoryBot.define do
     account { Faker::Lorem.characters(number: 10) }
     email { Faker::Internet.email }
     password { 'password' }
+
+    trait :with_image do
+      after(:build) do |user|
+        file_path = Rails.root.join('spec/fixtures/files/test-image.png')
+        user.avatar.attach(
+          io: File.open(file_path),
+          filename: 'test-image.png',
+          content_type: 'image/png'
+        )
+      end
+    end
+
   end
+
 end
